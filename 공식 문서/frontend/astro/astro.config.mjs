053a2b2014c058522bbb7ff884e87a46b2905171
Lucid (NoHack) <nohack.dev@gmail.com>
2025-11-27 03:19:16 +0900

    [fe/astro] Syntax Highlighting 학습

--- 공식 문서/frontend/astro/astro.config.mjs
+++ 공식 문서/frontend/astro/astro.config.mjs
@@ -1,9 +1,22 @@
 // @ts-check
 import { defineConfig } from "astro/config";
+import expressiveCode from "astro-expressive-code";
+
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
   prefetch: {
     defaultStrategy: "viewport",
   },
+  integrations: [
+    expressiveCode({
+      themes: ["dracula-soft"],
+      defaultLocale: "ko-KR",
+      styleOverrides: {
+        // codeFontFamily: "Pretendard",
+      },
+    }),
+    mdx(),
+  ],
 });
