edcf2da12e7165b5c9700e7159437e87a6452324
Lucid (NoHack) <nohack.dev@gmail.com>
2025-12-01 05:43:43 +0900

    [npm/zod] Basic usage 학습

--- 공식 문서/npm/zod/src/basic-usage.ts
+++ 공식 문서/npm/zod/src/basic-usage.ts
@@ -1,15 +1,15 @@
 import * as z from 'zod';
 
-const Player = z.object({
+const PlayerSchema = z.object({
   username: z.string(),
   xp: z.number(),
 });
 
-type PlayerType = z.infer<typeof Player>;
+type Player = z.infer<typeof PlayerSchema>;
 
 // 에러 핸들링 방법 1
 try {
-  console.log(Player.parse({ username: 'billie', xp: '100' }));
+  console.log(PlayerSchema.parse({ username: 'NoHack', xp: '100' }));
 } catch (error) {
   if (error instanceof z.ZodError) {
     console.log(error.issues);
@@ -17,10 +17,15 @@ try {
 }
 
 // 에러 핸들링 방법 2 - Safe Parse
-const errorParsed = Player.safeParse({ username: 'billie', xp: 100 });
+const errorParsed = PlayerSchema.safeParse({ username: 'NoHack', xp: 100 });
 
 if (errorParsed.success) {
   console.log(errorParsed.data);
 } else {
   console.log(errorParsed.error.issues);
 }
+
+const player1: Player = {
+  username: 'NoHack',
+  xp: 100,
+};
